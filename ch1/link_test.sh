52e1f3770cccee036cbbf8fc3031ce92f7c018f5
Bon-gyu KOO <protocolstack9@gmail.com>
2013-02-17 23:36:32 +0900

    ch1/link script modified.
    Signed-off-by: Bon-gyu KOO <protocolstack9@gmail.com>

--- ch1/link_test.sh
+++ ch1/link_test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 echo "... make symbolic link"
-ln -s $0 $0-sym
+ln -s $0 $0-sym 2>/dev/null
 
 echo "... make hard-link"
-ln $0 $0-hard
+ln $0 $0-hard 2>/dev/null
 
 ls -i $0 $0-sym $0-hard
+
+ls -l
+echo ""
+echo "           ^ check second column of output"
