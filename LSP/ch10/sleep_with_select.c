2da2fcb5b76a1ec94a2c77e09b332701c98d59a6
freestyle <protocolstack9@gmail.com>
2013-03-22 17:00:10 +0900

    20130322: private: work...
    Signed-off-by: freestyle <protocolstack9@gmail.com>

--- LSP/ch10/sleep_with_select.c
+++ LSP/ch10/sleep_with_select.c
@@ -9,15 +9,12 @@ void alarm_handler (int signo)
 {
 	printf ("[+] %s\n", __func__);
 	printf ("[-] %s\n", __func__);
+	fflush (stdout);
 }
 
 int main (void)
 {
-	struct timeval tv;
-	struct sigaction alarm_act;
-
-	int ret;
-
+#if 0
 #if 0
 	struct sigaction {
 		void     (*sa_handler)(int);
@@ -28,6 +25,32 @@ int main (void)
 	};
 #endif
 
+	struct timeval tv;
+	int ret;
+
+	/* SIGALRM에 의해 select가 방해 받는 예제 */
+	alarm (1);
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+
+	ret = select (0, NULL, NULL, NULL, &tv);
+
+	if (ret == 0) {
+		printf ("time-out\n");
+	}
+	if (ret < 0) {
+		perror ("select");
+	}
+
+
+#else
+
+
+	struct sigaction alarm_act;
+	sigset_t sig;
+	int ret;
+
+
 	alarm_act.sa_handler = alarm_handler;
 	sigemptyset (&alarm_act.sa_mask);
 	alarm_act.sa_flags = 0;
@@ -37,42 +60,16 @@ int main (void)
 		return -1;
 	}
 
-
-#if 0
-	/* SIGALRM에 의해 select가 방해 받는 예제 */
-	alarm (1);
-	tv.tv_sec = 1;
-	tv.tv_usec = 0;
-
-	ret = select (0,NULL,NULL,NULL,&tv);
-
-	if (ret == 0) {
-		printf ("time-out\n");
-	}
-	if (ret < 0) {
-		perror ("select");
-	}
-#endif
-
-
-
-#if 1
-	if (sigaction (SIGALRM, &alarm_act, NULL) < 0) {
-		perror ("sigaction\n");
-		return -1;
-	}
-
-	sigset_t sig;
-	sigemptyset(&sig);
-	sigaddset(&sig, SIGALRM); // mask
+	sigemptyset (&sig);
+	sigaddset (&sig, SIGALRM); /* mask */
 
 	while(1) {
 		struct timespec ts;
 
 		alarm (1);
-		ts.tv_sec = 1;
-		ts.tv_nsec = 0;
-		int ret = pselect (0,NULL,NULL,NULL,&ts, &sig);
+		ts.tv_sec = 0;
+		ts.tv_nsec = 500000000;
+		int ret = pselect (0, NULL, NULL, NULL, &ts, &sig);
 
 		if (ret == 0) {
 			printf ("time-out\n");
