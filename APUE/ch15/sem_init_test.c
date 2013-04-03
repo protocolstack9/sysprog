d943fdc89c7d6bea9c82dbed25167d04e2ed9f84
freestyle <protocolstack9@gmail.com>
2013-04-03 17:23:51 +0900

    20130402: APUE: ipc update
    Signed-off-by: freestyle <protocolstack9@gmail.com>

--- APUE/ch15/sem_init_test.c
+++ APUE/ch15/sem_init_test.c
@@ -1,6 +1,16 @@
+/*
+ *  top -p [waiter's pid]
+ *    waiting process state is 'S'.
+ *      when attain semaphore, state is changed to 'R'.
+ */
 #include <stdio.h>
 #include <semaphore.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/mman.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
 sem_t *sem;
@@ -9,7 +19,7 @@ int main (void)
 {
 	int fd;
 
-	sem = mmap(0, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	sem = mmap (0, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
 	if (0 > sem_init (sem, 1, 1)) {
 		perror ("sem_init");
@@ -25,19 +35,29 @@ int main (void)
 	}
 	/* child */
 	else if (!fd) {
+		printf ("[child] pid: %d\n", getpid ());
 		sem_wait (sem);
 		printf ("[%d] hello, world!\n", getpid ());
 		fflush (stdout);
-		sleep (10);
+		sleep (7);
 		sem_post (sem);
+
+		exit (0);
 	}
 	/* parent */
 	else {
+		int i = 0;
+		int status = 0;
+
+		printf ("[parent] pid: %d\n", getpid ());
+		sleep(1);
 		sem_wait (sem);
 		printf ("[%d] hello, world!\n", getpid ());
 		fflush (stdout);
-		sleep (10);
 		sem_post (sem);
+
+		waitpid (fd, &status, 0);
+		while (++i) { }
 	}
 
 	sem_destroy (sem);
